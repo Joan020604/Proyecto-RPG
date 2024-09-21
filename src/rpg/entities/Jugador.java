d5a9c1cc63ee9304bd97d5d1c3976e7543df7a65
Joan020604 <l22280947@toluca.tecnm.mx>
2024-09-20 22:00:07 -0600

    Initial commit

--- src/rpg/entities/Jugador.java
+++ src/rpg/entities/Jugador.java
@@ -1,5 +1,7 @@
 package rpg.entities;
 
+import rpg.enums.Stats;
+
 import java.util.HashMap;
 
 public class Jugador {
@@ -10,8 +12,8 @@ public class Jugador {
         public Jugador(String name){
             this.name= name;
             this.stats= new HashMap<>();
-            this.stats.put(Stats.MAX_HP,100);
-            this.stats.put(Stats.HP,100);
+            this.stats.put(Stats.MAX_HP,200);
+            this.stats.put(Stats.HP,200);
             this.stats.put(Stats.MAX_MP,100);
             this.stats.put(Stats.MP,100);
             this.stats.put(Stats.ATAQUE,30);
@@ -19,7 +21,7 @@ public class Jugador {
         }
 
 
-        public boolean isAlive(){
+    public boolean isAlive(){
             return stats.get(Stats.HP)>0;
         }
         public void ataque(Enemy enemy){
@@ -30,8 +32,13 @@ public class Jugador {
                 System.out.println(enemy.getName() + "a perdido" + enemy.getStats().GET(Stats.HP)+ " de HP.");
             }
             else {
-                System.out.println(this.name + " ataco " + enemy.getName() + "pero no hizo daño");}
+                System.out.println(this.name + " ataco " + enemy.getName() + "pero no le hizo daño");}
+        }
+        public String getName(){
+            return name;
+        }
+        public HashMap<Stats, Integer> getStats(){
+            return stats;
         }
     }
 
-}
