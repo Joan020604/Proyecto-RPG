95bd5acc995794cbbec8ba0469a9273cf466afbe
Joan020604 <l22280947@toluca.tecnm.mx>
2024-10-22 13:17:26 -0600

    Initial commit

---
+++ src/rpg/enums/ArmorType.java
@@ -0,0 +1,17 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package rpg.enums;
+
+public enum ArmorType {
+    HELMET,
+    CHESTPLATE,
+    LEGGINGS,
+    BOOTS,
+    HANDS;
+
+    private ArmorType() {
+    }
+}
