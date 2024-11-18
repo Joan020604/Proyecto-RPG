d132705e6a7f0ea1dcec370443e08601db5ac530
Arturo Emmanuel Espinosa Sánchez <L22281329@toluca.tecnm.mx>
2024-11-18 10:02:14 -0600

    GameCharacter.java

--- src/rpg/entities/GameCharacter.java
+++ src/rpg/entities/GameCharacter.java
@@ -12,23 +12,50 @@ import java.util.HashMap;
  * Esta clase debe ser extendida por clases concretas que implementen el método `initCharacter`.
  */
 public abstract class GameCharacter {
-
+    
+   /** Nombre del personaje 
+    */
     protected String name;
-
+    
+   /** Estadísticas del personaje representadas como un mapa de Stats a enteros 
+    */
     protected HashMap<Stats, Integer> stats;
 
+   /**
+    * Constructor de GameCharacter.
+    * Inicializa el nombre del personaje y crea un mapa vacío para las estadísticas.
+    * 
+    * @param name el nombre del personaje
+    */
     public GameCharacter(String name) {
 
         this.name = name;
         this.stats = new HashMap<>();
     }
 
+   /**
+    * Método abstracto para inicializar las características del personaje.
+    * Debe ser implementado por las subclases para configurar las estadísticas iniciales.
+    */
     protected abstract void initCharacter();
 
+   /**
+    * Verifica si el personaje está vivo.
+    * Un personaje se considera vivo si sus puntos de vida (HP) son mayores a 0.
+    * 
+    * @return true si el personaje tiene HP mayor a 0, de lo contrario false.
+    */
     public boolean isAlive() {
         return stats.get(Stats.HP) > 0;
     }
 
+   /**
+    * Realiza un ataque a otro personaje.
+    * Calcula el daño infligido basándose en las estadísticas de ataque y defensa, 
+    * actualiza los puntos de vida (HP) del enemigo y muestra un mensaje con los resultados del ataque.
+    * 
+    * @param enemy el personaje enemigo que recibe el ataque
+    */
     public void attack(GameCharacter enemy) {
 
         String message = "";
@@ -54,10 +81,20 @@ public abstract class GameCharacter {
         System.out.println(message);
     }
 
+   /**
+    * Obtiene el nombre del personaje con un título decorativo.
+    * 
+    * @return el nombre del personaje en formato "Nombre el Intrépido".
+    */
     public String getName() {
         return String.format("%s el Intrépido", name);
     }
 
+    /**
+    * Obtiene las estadísticas del personaje.
+    * 
+    * @return un mapa que representa las estadísticas del personaje.
+    */
     public HashMap<Stats, Integer> getStats() {
         return stats;
     }
