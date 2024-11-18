ef06a1bc79f2abf612dcc318bca3690698b6ba76
Arturo Emmanuel Espinosa Sánchez <L22281329@toluca.tecnm.mx>
2024-11-18 10:18:40 -0600

    EnemyType.java

--- src/rpg/enums/EnemyType.java
+++ src/rpg/enums/EnemyType.java
@@ -1,8 +1,20 @@
 package rpg.enums;
 
+/**
+ * Enumeración que representa los diferentes tipos de enemigos en el juego RPG.
+ * Cada tipo de enemigo indica su nivel de dificultad o rareza.
+ */
 public enum EnemyType {
+
+    /** Enemigo básico: nivel inicial, comúnmente más débil y fácil de derrotar */
     BASIC,
+
+    /** Enemigo medio: nivel intermedio, presenta mayor desafío que un enemigo básico */
     MEDIUM,
+
+    /** Jefe: enemigo poderoso con estadísticas superiores y habilidades especiales */
     BOSS,
-    SECRET
+
+    /** Enemigo secreto: rara vez encontrado, puede tener habilidades únicas o recompensas especiales */
+    SECRET;
 }
