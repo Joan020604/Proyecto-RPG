99a9c3415beff179068a1e8c9037762ec8d3a96e
Arturo Emmanuel Espinosa Sánchez <L22281329@toluca.tecnm.mx>
2024-11-18 10:28:32 -0600

    WeaponType.java

--- src/rpg/enums/WeaponType.java
+++ src/rpg/enums/WeaponType.java
@@ -1,17 +1,38 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
+/**
+ * Enumeración que representa los diferentes tipos de armas disponibles
+ * en el juego (RPG).
+ */
 package rpg.enums;
 
 public enum WeaponType {
+    /**
+     * Espada: arma equilibrada con buena capacidad de ataque.
+     */
     SWORD,
+
+    /**
+     * Hacha: arma poderosa con mayor daño pero menos precisión.
+     */
     AXE,
+
+    /**
+     * Arco: arma de ataque a distancia con alta velocidad.
+     */
     BOW,
+
+    /**
+     * Bastón: arma mágica utilizada principalmente por magos o sanadores.
+     */
     STAFF,
+
+    /**
+     * Grimorio: libro mágico que permite lanzar hechizos avanzados.
+     */
     GRIMOIRE;
 
+    /**
+     * Constructor privado de la enumeración.
+     */
     private WeaponType() {
     }
 }
